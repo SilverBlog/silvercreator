0ce91c147f7145bf4859a2df7cd70e69a6e31f6d
Yukino Song <syqlds@126.com>
2017-09-06 14:42:05 +0800

    Improve autosave

--- src/article_block.js
+++ src/article_block.js
@@ -1,11 +1,13 @@
 import tpl from './article_block.eft'
 import style from './style.css'
 import getKey from './get_key.js'
-import popAlert from './alert.js'
+import { popAlert, hideAlert } from './alert.js'
 import { getPosts } from './loader.js'
 import edit from './editor.js'
 import md5 from 'blueimp-md5'
 import axios from 'axios'
+
+import gState from './state.js'
 
 const editPost = ({state: {$data}, value}) => {
 	const saved = localStorage.getItem(`smde_${$data.name}`)
@@ -17,16 +19,23 @@ const editPost = ({state: {$data}, value}) => {
 			saved
 		})
 	}
+	if (gState.fetching) return popAlert('Please wait...')
+	gState.fetching = true
 	axios.post(`${localStorage.getItem('site')}/control/get_post_content`, {
 		'post_id': parseInt(value, 10)
 	})
 	.then(resp => resp.data)
-	.then(data => edit({
-		type: 'post',
-		index: value,
-		data
-	}))
+	.then((data) => {
+		hideAlert()
+		gState.fetching = false
+		edit({
+			type: 'post',
+			index: value,
+			data
+		})
+	})
 	.catch((err) => {
+		gState.fetching = false
 		popAlert(err.message)
 	})
 }
@@ -36,16 +45,21 @@ const deletePost = ({state, value}) => {
 		const del = (key) => {
 			const salt = `${value}${state.$data.title}`
 			const encode = md5(`${salt}${key}`)
+			if (gState.fetching) return popAlert('Please wait...')
+			gState.fetching = true
 			axios.post(`${localStorage.getItem('site')}/control/delete`, {
 				"post_id": parseInt(value, 10),
 				encode
 			})
 			.then(resp => resp.data)
 			.then((data) => {
+				gState.fetching = false
 				if (data.status) return getPosts(() => popAlert('Deleted successfully.'))
 				popAlert('Wrong password, please try again.', () => getKey(del))
 			})
 			.catch((err) => {
+				hideAlert()
+				gState.fetching = false
 				popAlert(err.message)
 			})
 		}
